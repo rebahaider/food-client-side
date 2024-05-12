65ead9b3f33e97085d33daef1d34e19e7af93253
rebahaider <rebahaiderema@gmail.com>
2024-05-13 02:23:54 +0600

    show all button's route set up

--- src/Pages/Home/Home/FeaturedFoodCard.jsx
+++ src/Pages/Home/Home/FeaturedFoodCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const FeaturedFoodCard = ({ food }) => {
@@ -32,7 +33,7 @@ const FeaturedFoodCard = ({ food }) => {
                         </button>
                         <button className="px-5 py-2.5 relative rounded group overflow-hidden font-medium bg-purple-50 text-red-600 inline-block">
                             <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-red-600 group-hover:h-full opacity-90"></span>
-                            <span className="relative group-hover:text-white font-extrabold">Show All</span>
+                            <Link to="/availableFoods"><span className="relative group-hover:text-white font-extrabold">Show All</span></Link>
                         </button>
 
                     </div>
