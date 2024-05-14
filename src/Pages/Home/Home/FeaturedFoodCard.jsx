e6a8856def8d8bc82dfab3d0fc586bc458309f6b
rebahaider <rebahaiderema@gmail.com>
2024-05-14 22:59:57 +0600

    fixed some private routing

--- src/Pages/Home/Home/FeaturedFoodCard.jsx
+++ src/Pages/Home/Home/FeaturedFoodCard.jsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 
 const FeaturedFoodCard = ({ food }) => {
-    const { image, name, quantity, expired_date_time, additional_notes, donator_image, donator_name, pickup_location } = food;
+
+    const { user } = useContext(AuthContext);
+    const { _id, image, name, quantity, expired_date_time, additional_notes, donator_image, donator_name, pickup_location } = food;
     return (
         <section className="bg-gray-200 text-gray-800 rounded-2xl">
             <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
@@ -29,7 +33,7 @@ const FeaturedFoodCard = ({ food }) => {
 
                         <button className="px-5 py-2.5 relative rounded group overflow-hidden font-medium bg-purple-50 text-red-600 inline-block">
                             <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-red-600 group-hover:h-full opacity-90"></span>
-                            <Link to="/login"><span className="relative group-hover:text-white font-extrabold">View Detail</span></Link>
+                            <Link to={user ? `/singlePage/${_id}` : '/login'}><span className="relative group-hover:text-white font-extrabold">View Detail</span></Link>
                         </button>
                         <button className="px-5 py-2.5 relative rounded group overflow-hidden font-medium bg-purple-50 text-red-600 inline-block">
                             <span className="absolute top-0 left-0 flex w-full h-0 mb-0 transition-all duration-200 ease-out transform translate-y-0 bg-red-600 group-hover:h-full opacity-90"></span>
